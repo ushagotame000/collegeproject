e1fce29aab8061ae22dd2b2342f057c542237099
unknown <dracokd19@gmail.com>
2024-06-20 05:24:00 +0545

    Set layout for the sideBar

--- client/src/components/RightBar.tsx
+++ client/src/components/RightBar.tsx
@@ -2,9 +2,18 @@ import React from "react";
 
 function RightBar() {
   return (
-    <>
-      <div className=" bg-black text-white h-full">RightBar</div>
-    </>
+    <div className="bg-black text-white h-full w-60 flex flex-col items-center justify-evenly ">
+      <div className=" flex-cols grow-0">
+        <div className="rounded-full h-24 w-24 bg-gray-400 flex items-center justify-center">
+          <span className="text-lg">Image</span>
+        </div>
+        <div>Contact Name</div>
+      </div>
+      <div className="flex-cols w-full h-full p-2 justify-between grow">
+        <div>Medias</div>
+        <div>Files</div>
+      </div>
+    </div>
   );
 }
 
