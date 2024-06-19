04e93407e69e34a3de3ce1367843502ac97db983
unknown <dracokd19@gmail.com>
2024-06-20 05:32:50 +0545

    Set layout

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function Chat() {
   return (
-    <>
-      <div className="flex-grow">ChatBox</div>
-    </>
+    <div className="flex flex-col h-full flex-grow">
+      <div className=" bg-black text-white p-2">Top Menu</div>
+      <div className="flex-grow overflow-y-auto bg-gray-200 p-2">Messages</div>
+      <div className="bg-black text-white p-4">Send</div>
+    </div>
   );
 }
 
