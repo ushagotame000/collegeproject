8cda115e46ed099bd54abb70d5a5c3e11f3e2c1a
Usha Gotame <ushagotame000@gmail.com>
2024-06-20 14:56:38 +0545

    signup/login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import LoginPopup from "./components/login form/LoginPopup";
+// import { useState } from "react";
 function App() {
+  // const [showLogin, setShowLogin] = useState(false);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<LoginPopup />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/assets/assets.js
@@ -0,0 +1,13 @@
+import cross_icon from './cross_icon.png'
+
+
+
+
+
+
+
+
+
+export const assets = {
+    cross_icon
+}
