f1135856e844acb884e471715545b16f14ef4580
unknown <dracokd19@gmail.com>
2024-06-24 16:50:11 +0545

    fixes

--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -1,5 +1,5 @@
-import React, { useState, ChangeEvent } from 'react';
-import { assets } from '../../assets/assets';
+import React, { useState, ChangeEvent } from "react";
+import { assets } from "../../assets/assets";
 
 const Profile: React.FC = () => {
   const [photo, setPhoto] = useState<string>(assets.profile);
@@ -14,7 +14,11 @@ const Profile: React.FC = () => {
     <>
       <div className="profile-container justify-center flex mt-14 bg-slate-500 ">
         <div className="profile relative">
-          <img src={photo} alt="profile" className="w-44 h-44 rounded-full" />
+          <img
+            src={photo}
+            alt="profile"
+            className="w-44 h-44 rounded-full object-cover"
+          />
           <input
             type="file"
             id="photo-upload"
@@ -22,14 +26,13 @@ const Profile: React.FC = () => {
             accept="image/*"
             onChange={handlePhotoChange}
           />
-           <label htmlFor="photo-upload">
+          <label htmlFor="photo-upload">
             <img
               src={assets.camera}
               alt="camera"
               className="w-10  ml-28 -mt-16 bg-white rounded-full p-2 cursor-pointer fixed"
             />
-           </label>
-        
+          </label>
         </div>
       </div>
     </>
