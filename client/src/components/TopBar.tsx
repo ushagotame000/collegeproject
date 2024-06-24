00b72166348f7a296db3f87884252717143ba128
Usha Gotame <ushagotame000@gmail.com>
2024-06-24 16:37:13 +0545

    resp

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -9,20 +9,43 @@ function TopBar() {
 
   return (
     <>
-      <div className="nav bg-black h-10 text-white w-full flex justify-end items-center border-b">
+      <div className="nav bg-black h-10 text-white w-full flex justify-between border-b">
+      <div className="searchBar relative mb-2 ">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <i className="fas fa-search text-white"></i>
+          </div>
+          <input
+            type="text"
+            name="searchContact"
+            id="searchContact"
+            className="w-full pl-9 pr-3 py-2 rounded-2xl bg-gray-400 h-9 placeholder:text-white text-sm focus:outline-none"
+            placeholder="Search Contacts...."
+            
+          />
+        </div>
+    
+    <div className="2">
+
+      
+  
+      {/* setting icon */}
+      
         <button className="mr-2 focus:outline-none">
           <i className="fas fa-cog"></i>
         </button>
+
+        {/* notification icon */}
         <button className="mr-2 focus:outline-none">
           <i className="fas fa-bell"></i>
         </button>
+
+        {/* usericon with dropdown */}
         <button
           className="mr-2 focus:outline-none"
           onClick={handleUserIconClick}
         >
           <i className="fas fa-user"></i>
         </button>
-<<<<<<< HEAD
         {showOptions && (
           <div className="absolute right-2 top-8 mt-2  bg-transparent   bg-violet-500 rounded shadow-lg">
             <button className="block w-full text-left py-2 px-4 hover:bg-violet-600 rounded focus:outline-none">
@@ -31,22 +54,9 @@ function TopBar() {
             <button className="block w-full text-left py-2 px-4 mt-2  hover:bg-violet-600 rounded focus:outline-none">
               Log out
             </button>
-=======
-        {showLogout && (
-          <div className="absolute right-2 top-8 mt-2 flex-col bg-violet-600">
-            <div className="border-b border-black">
-              <button className="py-2 px-4  hover:bg-violet-800 rounded shadow-lg focus:outline-none active:bg-violet-800">
-                Profile
-              </button>
-            </div>
-            <div>
-              <button className="py-2 px-4 bg-violet-600 hover:bg-violet-800 rounded shadow-lg focus:outline-none active:bg-violet-800">
-                Log out
-              </button>
-            </div>
->>>>>>> e0c1eb99aabf79ae2694cee072e15b52f8c3d0c1
           </div>
         )}
+          </div>
       </div>
     </>
   );
